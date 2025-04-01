4ac30a13995a1faad70e2da40316b0b0579737b4
Lans12345 <olanalans12345@gmail.com>
2025-04-02 06:42:12 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:word_tales/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WordTales',
+      home: const HomeScreen(),
+    );
+  }
+}
